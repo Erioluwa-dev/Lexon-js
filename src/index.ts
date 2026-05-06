1631c0a0d0f03b3d89e2fa92fd2c9fb174a0bd7e
dev dami <dev.dami@example.com>
2026-05-06 21:40:09 +0100

    feat: add public API exports

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { string } from "./schema/string";
+export { StringBuilder } from "./schema/builder";
+
+export type {
+  CompiledSchema,
+  ExplainResult,
+  StringAstNode,
+  MinNode,
+  MaxNode,
+  CharsetNode,
+  NoSpacesNode,
+  AstNode,
+  AstNodeType,
+  ValidationResult,
+  SchemaOptions
+} from "./core/types";
+
+export { compile, type CompilerOptions } from "./core/compiler";
+
+export { explain } from "./debug/explain";
